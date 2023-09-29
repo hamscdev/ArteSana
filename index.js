9868fe69968d73aa1d1bd06609a62fc757ef55d7
hector moreno <hectoraguilas93@gmail.com>
2023-09-28 19:16:57 -0600

    configuration swagger ui

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const express = require('express')
+const express = require("express")
+const cors = require("cors")
 const app = express()
-const port = 3000;
+
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+const routeUser = require("./routes/routes_user")
+const port = process.env.PORT || 3000
+
+
+const options = {
+    definition: {
+        openapi: "3.0.0", 
+        info: {
+            title: "ArteSana API",
+            version: "1.0.0",
+            description: "a ArteSana API Library"
+        }, 
+        servers:[{
+            url: "https://localhost:3000"
+        }]
+        
+    },
+    apis:["./routes/*.js"]
+}
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(specs));
 
 
 
-
-
+app.use(cors)
+app.use(express.json())
 
 
 
